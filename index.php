fba8c94221ef37606dc1de47975b848db9e4717a
fede.toldo96@gmail.com <fede.toldo96@gmail.com>
2022-03-24 11:38:57 +0100

    latest news

--- index.php
+++ index.php
@@ -13,39 +13,41 @@
         </div>
     </div>
     <section class="loop mb-160-r">
-        <?php 
-            $loop = new WP_Query( array(
-                'post_type'         => 'service-project',
+    <?php 
+            $loop = new WP_Query( array( 
+                'post_type'         => 'post',
                 'post_status'       => 'publish',
                 'orderby'           => 'count',
                 'order'             => 'DESC',
-                'posts_per_page'    => 3,
-                'paged' => get_query_var('paged') ? get_query_var('paged') : 1) 
-            ); 
-            
-            while ($loop -> have_posts()) : $loop -> the_post(); ?>
+                'posts_per_page'    => 3
+            ) );
+            if ($loop->have_posts()) : while($loop->have_posts()) : $loop->the_post(); ?>
                 <article>
-                    <div class="row">
-                        <div class="col-12 order-lg-2 col-lg-4">
-                            <a href="<?php the_permalink();?>" class="loop__img-permalink">
-                                <img src="<?php echo get_the_post_thumbnail_url();?>" alt="<?php echo $thumbnail_alt ?>">
-                            </a>
-                        </div>
-                        <div class="col-12 col-lg-8">
-                            <div class="loop__content-container">
-                                <div class="loop__over-content">
-                                    <p class="project-client mb-8-r"><?php echo carbon_get_post_meta( get_the_ID(), 'crb_project_client' ); ?></p>
-                                </div>
-                                <div class="loop__content">                                 
-                                    <h3 class="mb-16-r"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
-                                    <p class="mb-16-r"><?php echo get_the_excerpt(); ?></p>
-                                    <a class="read-more" href="<?php the_permalink();?>">Read more</a>
+                    
+                        <div class="row">
+                            <div class="col-12 order-lg-2 col-lg-4">
+                                <a href="<?php the_permalink();?>" class="loop__img-permalink">
+                                    <img src="<?php echo get_the_post_thumbnail_url();?>" alt="<?php echo $thumbnail_alt ?>">
+                                </a>
+                            </div>
+                            <div class="col-12 col-lg-8">
+                                <div class="loop__content-container">
+                                    <div class="loop__over-content">
+                                        <p class="mb-8-r"><?php echo get_the_date(); ?></p>
+                                    </div>
+                                    <div class="loop__content">                                 
+                                        <h3 class="mb-16-r"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h3>
+                                        <p class="mb-16-r"><?php echo get_the_excerpt(); ?></p>
+                                        <a class="read-more" href="<?php the_permalink();?>">Read more</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>        
                 </article>
-            <?php endwhile; ?>
+                <?php endwhile; ?>
+            <?php else : ?>
+                <p> <?php esc_html_e( 'Sorry ma non ci sono post che corrispondono a questo criterio', 'slug-theme' ); ?> </p>
+            <?php endif; ?>
 
         <div class="pagination">
             <?php
